c41f15867c67ec0f6939ad4d2ad04990a0bdef90
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-14 07:31:27 +0100

    Updated README.md

---
+++ README.md
@@ -0,0 +1,41 @@
+# Weather App
+This is the code for the weather application.
+
+## Description
+There will be different branches of this project and every branch will cover a basic concept 
+of kotlin in terms of the weather app.
+
+## Technical
+* Kotlin 1.8
+* JVM Version 1.7.20
+* Gradle 7.4.2
+* IntelliJ IDEA
+
+## Installation
+**1. Required to run project:**
+- To run this project use IntelliJIDEA.
+- Gradle version used: gradle 2.0.
+
+**2. Clone this repository :**
+
+         `git clone https://github.com/Sonal3274/WeatherApp.git`
+
+**3. open Project in intelliJIDEA**
+
+**4. Build Project**
+
+**5. In case of an error when building project, update your gradle version, Build Tools download**
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Feature to be added
+- [ ] Add different branches for every different kotlin concept
+- [ ] Tests
+- [ ] Fix Gradle
+- [ ] Add github actions
+
+
+
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,62 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+//enum class Weather {
+//    SUNNY, CLOUDY, RAINY, SNOWY
+//}
+//
+//fun main() {
+//    val currentWeather = Weather.CLOUDY
+//
+//    when (currentWeather) {
+//        Weather.SUNNY -> println("It's sunny outside, wear sunscreen.")
+//        Weather.CLOUDY -> println("It's cloudy outside, bring an umbrella.")
+//        Weather.RAINY -> println("It's raining outside, wear a raincoat.")
+//        Weather.SNOWY -> println("It's snowing outside, wear warm clothes.")
+//    }
+//}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/*
+class Weather(val temperature: Double, val condition: String) {
+    fun getAdvice(): String {
+        return when (condition) {
+            "sunny" -> "It's sunny outside, wear sunscreen."
+            "cloudy" -> "It's cloudy outside, bring an umbrella."
+            "rainy" -> "It's raining outside, wear a raincoat."
+            "snowy" -> "It's snowing outside, wear warm clothes."
+            else -> "I'm not sure what the weather is like."
+        }
+    }
 }
+
+object WeatherData {
+    fun getWeather(): Weather {
+
+        val temperature = 20.0
+        val condition = "sunny"
+        return Weather(temperature, condition)
+    }
+}
+*/
+
+fun main() {
+
+    val temp1 = 20.0
+    val temp2 = 35.2
+    println("The sum of $temp1 and $temp2 is ${temp1 + temp2}")
+}
+
+
+
+
+
+/*  var currentWeather = WeatherData.getWeather()
+  println("Temperature: ${currentWeather.temperature}")
+  println("Advice: ${currentWeather.getAdvice()}")*/
+
+
+
+/*In this example, we have defined a class called Weather that takes in the temperature and condition of the weather.
+ We also have an getAdvice method that returns a string with advice based on the weather condition.
+ We then have an object called WeatherData which has a getWeather method that returns an instance of Weather.
+ In the main function, we call getWeather to get the current weather and then output the temperature and advice.
+ The use of an object in this example makes it easy to reuse the getWeather method in other parts of the app.
+*/
+
